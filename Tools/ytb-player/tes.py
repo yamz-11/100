618c8fc91c7716a5dba2d0492e06043384b8edfd
Rusmana-ID <41493567+Rusmana-ID@users.noreply.github.com>
2019-08-02 23:02:38 +0700

    Add files via upload

---
+++ Tools/ytb-player/tes.py
@@ -0,0 +1,11 @@
+import os, sys
+
+def pilih():
+	os.system("ytb "+ serch)
+	
+	
+serch = raw_input("\n\033[39;1m[\033[32;1m+\033[39;1m]MASUKAN JUDUL LAGU\033[31;1m:\033[32;1m ")
+print "wait..."
+
+pilih()
+	
